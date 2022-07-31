4f9a80750a073390fa8cef668305e0bb10c3a9fb
chekin <santiisalcedo129@gmail.com>
2022-07-31 00:54:37 -0300

    try deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,9 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:SalcedoSantiago/porfolio.git main:gh-pages
 

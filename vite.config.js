6841de340de60c0bde703aa4acb25b098da921fd
chekin <santiisalcedo129@gmail.com>
2022-08-29 09:28:11 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://c1801477.ferozo.com/",
+  base: "https://salcedosantiago.com",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,

64dc76c618f23eb5315945578c32b386fc7e63cb
Santiago Salcedo <65971415+SalcedoSantiago@users.noreply.github.com>
2024-04-15 01:02:40 -0300

    run build

--- src/App/Screens/Contact.jsx
+++ src/App/Screens/Contact.jsx
@@ -1,69 +1,61 @@
 /**
  * External dependencies
  */
-import React from 'react'
-import { Stack, Text, Heading, Flex, Button, } from '@chakra-ui/react';
-import { Element } from 'react-scroll'
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import { Stack, Text, Heading, Flex, Button } from "@chakra-ui/react";
+import { Element } from "react-scroll";
 
 /**
  * Internal dependencies
  */
 
 const Contact = () => {
-    return (
-        <Element
-            name='contact'
+  return (
+    <Element name="contact">
+      <Stack py={"150px"} justify="center" align={"center"} spacing={6}>
+        <Heading
+          display={"block"}
+          w={"100%"}
+          pb={2}
+          fontSize={"5xl"}
+          color="gray.100"
+          textAlign={"center"}
         >
-            <Fade bottom>
-                <Stack
-                    py={'150px'}
-                    justify='center'
-                    align={'center'}
-                    spacing={6}
-                >
-                    <Heading
-                        display={'block'}
-                        w={'100%'}
-                        pb={2}
-                        fontSize={'5xl'}
-                        color="gray.100"
-                        textAlign={'center'}
-                    >
-                        Get in touch
-                    </Heading>
-                    <Text
-                        maxW={'600px'}
-                        textAlign='center'
-                        lineHeight={'35px'}
-                        letterSpacing='0.2px'
-                        fontSize={'18px'}
-                        color="gray.300"
-                        pb={'30px'}
-                    >I'm currently actively looking for new opportunities and projects<br />
-                        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
-                    </Text>
+          Get in touch
+        </Heading>
+        <Text
+          maxW={"600px"}
+          textAlign="center"
+          lineHeight={"35px"}
+          letterSpacing="0.2px"
+          fontSize={"18px"}
+          color="gray.300"
+          pb={"30px"}
+        >
+          I'm currently actively looking for new opportunities and projects
+          <br />
+          Whether you have a question or just want to say hi, I’ll try my best
+          to get back to you!
+        </Text>
 
-                    <Flex py={2} gap={2}>
-                        <Button
-                            px={'50px'}
-                            variant="primary"
-                            borderRadius={'3px'}
-                            transition="all 0.2s ease"
-                            fontSize="16px"
-                            fontWeight={400}
-                            as='a'
-                            href="mailto:santiagosalcedod@gmail.com"
-                            letterSpacing={'0.20px'}
-                        >
-                            Say Hello!
-                        </Button>
-                    </Flex>
-                </Stack>
-            </Fade>
-        </Element>
+        <Flex py={2} gap={2}>
+          <Button
+            px={"50px"}
+            variant="primary"
+            borderRadius={"3px"}
+            transition="all 0.2s ease"
+            fontSize="16px"
+            fontWeight={400}
+            as="a"
+            href="mailto:santiagosalcedod@gmail.com"
+            letterSpacing={"0.20px"}
+          >
+            Say Hello!
+          </Button>
+        </Flex>
+      </Stack>
+    </Element>
+  );
+};
 
-    )
-}
-
-export default Contact
+export default Contact;
